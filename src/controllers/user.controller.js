21515393ef312c07072c1a0c654fdcae2196e83a
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-05-26 23:54:37 +0530

    updated types

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,9 @@
+/**
+ * 
+ * @param {import("express").Request} req 
+ * @param {import("express").Response} res 
+ * @param {import("express").NextFunction} next 
+ */
 export const createUser = async (req, res, next) => {
 	try {
 		
--- src/utils/response.js
+++ src/utils/response.js
@@ -5,7 +5,7 @@
  * @param {number} status - The HTTP status code for the response.
  * @param {string} message - A message describing the response.
  * @param {*} data - The data to include in the response body.
- * @returns {Object} - The JSON response object.{success: boolean, message: string, data: *}.
+ * @returns {object} - The JSON response object.{success: boolean, message: string, data: *}.
  */
 const response = (res, status, message, data) => {
 	const resObj = {
